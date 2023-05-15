81a9428d87e7bf99dd3320025449ec04f7c85034
Vincent Adeola <vince.adeola@outlook.com>
2023-05-15 21:04:07 +0100

    mvp completed

--- start_code/script.js
+++ start_code/script.js
@@ -1,1 +1,41 @@
 console.log("Testing, testing, 1 2 3 ...")
+
+let countriesData;
+
+const getAll = async () => {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const data = await response.json();
+  return data;
+};
+
+const setUp = async () => {
+  countriesData = await getAll();
+  listCountries();
+  const searchForm = document.querySelector("form");
+  searchForm.addEventListener("submit", search);
+};
+
+const listCountries = () => {
+  const list = document.querySelector("#countriesList");
+  list.innerHTML = "";
+
+  countriesData.forEach((country) => {
+    const countryList = document.createElement("li");
+    countryList.innerText = `Country: ${country.name.common}, Population: ${country.population}`;
+    list.appendChild(countryList);
+  });
+};
+
+const search = (event) => {
+  event.preventDefault();
+  const input = document.querySelector("#filter").value;
+  const filtered = countriesData.filter((country) =>
+    country.name.common.toLowerCase().includes(input.toLowerCase())
+  );
+  countriesData = filtered;
+  listCountries();
+};
+
+setUp();
+
+
